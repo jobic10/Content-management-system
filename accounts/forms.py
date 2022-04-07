d4fafd7e63804dd7ab61c62ca88d7d20c27b9611
hisboye <hisboye2104@gmail.com>
2022-04-07 09:03:40 +0100

    error in auth

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,43 +1,26 @@
 from dataclasses import fields
-<<<<<<< HEAD
-from django.forms import ModelForm, widgets
-from .models import Order, CustomUser
-=======
 from xml.dom import ValidationErr
 from django.forms import EmailField, ModelForm, widgets
 from .models import Order,CustomUser
->>>>>>> db3adf6 (error in forms.py)
 from django import forms
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password 
 
 
 class FormSetting(forms.ModelForm):
 
-    def __init__(self, *args, **kwargs):
-        super(FormSetting, self).__init__(*args, **kwargs)
+    def __init__(self, *args,**kwargs):
+        super(FormSetting,self).__init__(*args, **kwargs)
 
         for field in self.visible_fields():
-            field.field.widget.attrs['class'] = 'form-control'
+            field.field.widget.attrs['class']='form-control'
 
 
 class Create_Order(ModelForm):
     class Meta:
-        model = Order
-        fields = '__all__'
-
+        model=Order
+        fields='__all__'
 
 class CustomUserForm(FormSetting):
-<<<<<<< HEAD
-
-    email = forms.EmailField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput)
-    widget = {
-        'password': forms.PasswordInput(),
-    }
-
-    def __init__(self, *args, **kwargs):
-        super(CustomUserForm, self).__init__(*args, **kwargs)
-=======
     first_name=forms.CharField(max_length=100,label="First Name")
     last_name=forms.CharField(max_length=100,label="Last Name")
     email=forms.EmailField(required=True)
@@ -51,43 +34,20 @@ class CustomUserForm(FormSetting):
 
     def __init__(self, *args, **kwargs):
         super(CustomUserForm,self).__init__(*args,**kwargs)
->>>>>>> db3adf6 (error in forms.py)
         if kwargs.get('instance'):
-            instance = kwargs.get('instance').__dict__
-            self.fields['password'].required = False
-
+            instance=kwargs.get('instance').__dict__
+            self.fields['password'].required=False
+            
             for field in CustomUserForm.Meta.fields:
-                self.fields[field].initial = instance.get(field)
+                self.fields[field].initial=instance.get(field)
             if self.instance.pk is not None:
-<<<<<<< HEAD
-                self.fields['password'].widget.attrs['placeholder'] = "Enter your Password"
-=======
                 self.fields['password'].widget.attrs['placeholder']= "Enter your Password"
 
->>>>>>> db3adf6 (error in forms.py)
             else:
-                self.fields['first_name'].required = True
-                self.fields['last_name'].required = True
+                self.fields['first_name'].required=True
+                self.fields['last_name'].required=True
 
     def clean_email(self, *args, **kwargs):
-<<<<<<< HEAD
-        formEmail = self.cleaned_data['email'].lower()
-        if self.instance.pk is None:  # Insert
-            if CustomUser.objects.filter(email=formEmail).exists():
-                raise forms.ValidationError(
-                    "The given email is already registered")
-        else:  # Update
-            dbEmail = self.Meta.model.objects.get(
-                id=self.instance.pk).email.lower()
-            if dbEmail != formEmail:  # There has been changes
-                if CustomUser.objects.filter(email=formEmail).exists():
-                    raise forms.ValidationError(
-                        "The given email is already registered")
-        return formEmail
-
-    def clean_password(self):
-        password = self.cleaned_data.get('password', None)
-=======
         FormEmail=self.cleaned_data['email'].lower()
         if self.instance.pk is None:            #insert
             if CustomUser.objects.filter(email=FormEmail).exists():
@@ -110,20 +70,19 @@ class CustomUserForm(FormSetting):
             raise forms.ValidationError('password required')
         if password != password2:        
             raise forms.ValidationError('Password Mismatch')
->>>>>>> db3adf6 (error in forms.py)
         if self.instance.pk is None:
             if not password:
                 return self.instance.password
         return make_password(password)
 
-<<<<<<< HEAD
-=======
        
 
     
 
    
->>>>>>> db3adf6 (error in forms.py)
     class Meta:
-        model = CustomUser
-        fields = ['first_name', 'last_name', 'email', 'password']
+        model=CustomUser
+        fields=['first_name','last_name','email','password2']
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -11,155 +11,113 @@ from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
 from .email_backend import EmailBackend
 from django.contrib import messages
-<<<<<<< HEAD
-from django.contrib.auth import logout, login
-=======
 from django.contrib.auth import logout,login
 from django.contrib.auth.decorators import login_required
 
->>>>>>> db3adf6 (error in forms.py)
 
 
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
     # x=[randint(1,12) for p in range(0,1)]
-    customer = Customer.objects.all()
-    orders = Order.objects.all()
-    total_order = orders.count()
-    customers = customer.count()
-    orders_deliver = orders.filter(status='Delivered').count()
-    orders_pending = orders.filter(status='Pending').count()
+    customer=Customer.objects.all()
+    orders=Order.objects.all()
+    total_order=orders.count()
+    customers=customer.count()
+    orders_deliver=orders.filter(status='Delivered').count()
+    orders_pending=orders.filter(status='Pending').count()
 
-    context = {
+    context={
         # 'x':x,
-        'customer': customer,
-        'orders': orders,
-        'total_order': total_order,
-        'customers': customers,
-        'orders_deliver': orders_deliver,
-        'orders_pending': orders_pending
+        'customer':customer,
+        'orders':orders,
+        'total_order':total_order,
+        'customers':customers,
+        'orders_deliver':orders_deliver,
+        'orders_pending':orders_pending
     }
+    
+    return render(request,'dashboard.html',context)
 
-    return render(request, 'dashboard.html', context)
-
-
-def customer(request, pk_id):
+def customer(request,pk_id):
     try:
-        customer = Customer.objects.get(id=pk_id)
-        orders = customer.order_set.all()  # what does it do
-        total_orders = orders.count()
-        myFilter = OrderFilter(request.GET, queryset=orders)
-        orders = myFilter.qs
+        customer=Customer.objects.get(id=pk_id)
+        orders=customer.order_set.all()      #what does it do
+        total_orders=orders.count()
+        myFilter=OrderFilter(request.GET,queryset=orders)
+        orders=myFilter.qs
     except:
-        customer = {}
-        orders = {}
-        myFilter = {}
-        total_orders = ""
+        customer={}
+        orders={}
+        myFilter={}
+        total_orders=""
 
-    context = {
-        'customer': customer,
-        'orders': orders,
-        'total_orders': total_orders,
-        'myFilter': myFilter,
+    context={
+        'customer':customer,
+        'orders':orders,
+        'total_orders':total_orders,
+        'myFilter':myFilter,
     }
-
-    return render(request, 'customer.html', context)
-
+    
+    
+    return render(request,'customer.html',context)
 
 @login_required(login_url='login')
 def product(request):
-    products = Product.objects.all()
-    context = {
-        'products': products
+    products=Product.objects.all()
+    context={
+        'products':products
     }
-    return render(request, 'products.html', context)
+    return render(request,'products.html',context)
 
-<<<<<<< HEAD
-
-def orderform(request, pk):
-    OrderFormSet = inlineformset_factory(
-        Customer, Order, fields=('product', 'status'), extra=2)
-    customer = Customer.objects.get(id=pk)
-    formset = OrderFormSet(instance=customer)
-=======
 @login_required(login_url='login')
 def orderform(request,pk):
     OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=2)
     customer=Customer.objects.get(id=pk)
     formset=OrderFormSet(instance=customer)
->>>>>>> db3adf6 (error in forms.py)
     # form=Create_Order(initial={'customer':customer})
-    if request.method == "POST":
-        formset = OrderFormSet(request.POST, instance=customer)
+    if request.method=="POST":
+        formset=OrderFormSet(request.POST,instance=customer)
         # form=Create_Order(request.POST)
         if formset.is_valid():
             formset.save()
             return redirect('/')
-
-<<<<<<< HEAD
-    context = {
-        'formset': formset
+       
+    context={
+        'formset':formset
     }
-    return render(request, 'orderform.html', context)
+    return render(request,'orderform.html',context)
 
-
-def updates(request, pk):
-    order = Order.objects.get(id=pk)
-    form = Create_Order(instance=order)
-    if request.method == 'POST':
-        form = Create_Order(request.POST, instance=order)
-=======
 @login_required(login_url='login')
 def updates(request,pk):
     order=Order.objects.get(id=pk)  
     form=Create_Order(instance=order)
     if request.method=='POST':
         form=Create_Order(request.POST,instance=order)
->>>>>>> db3adf6 (error in forms.py)
         if form.is_valid():
             form.save()
             return redirect('/')
 
-    context = {
-        'form': form
+
+    context={
+        'form':form
 
     }
-    return render(request, 'orderform.html', context)
+    return render(request,'orderform.html',context)
 
-<<<<<<< HEAD
-
-def deleteOrder(request, pk):
-    order = Order.objects.get(id=pk)
-    if request.method == 'POST':
-=======
 @login_required(login_url='login')
 def deleteOrder(request,pk):
     order=Order.objects.get(id=pk)
     if request.method=='POST':
->>>>>>> db3adf6 (error in forms.py)
         order.delete()
         return redirect('/')
-    context = {
-        'item': order
+    context={
+        'item':order
     }
-<<<<<<< HEAD
-    return render(request, 'delete.html', context)
-
-
-def router(request):
-    if request.user.user_type == '1':
-        return redirect('dashboard')
-    else:
-        # Specifify the other page for the user e.g. return redirect('adminDashboard') but since you've not had that set up, I'll just redirect to the dashbaord you'd earlier created
-        return redirect('dashboard')
-
-=======
     return render(request,'delete.html',context)
 @login_required(login_url='login')
 def accountsettings(request):
     context={
->>>>>>> db3adf6 (error in forms.py)
 
     }
     return render(request,'account.html',context)
@@ -170,16 +128,6 @@ def cms(request):
 
 @unauthenticated
 def account_login(request):
-<<<<<<< HEAD
-    if request.user.is_authenticated:
-        return router(request)
-    if request.method == "POST":
-        user = EmailBackend.authenticate(request, username=request.POST.get(
-            'email'), password=request.POST.get('password'))
-        if user != None:
-            login(request, user)
-            return router(request)
-=======
     # if request.user.is_authenticated:
     #     if request.user.user_type=='1':
     #         return redirect('dashboard')
@@ -190,50 +138,31 @@ def account_login(request):
             login(request,user)
             if user.user_type == '1':
                 return redirect('dashboard')
->>>>>>> db3adf6 (error in forms.py)
 
             if user.user_type =='2':
                 return redirect('cms')
 
         else:
-<<<<<<< HEAD
-            messages.error(request, 'Invalid Login Parameters')
-            return redirect('/')
-
-    context = {}
-    return render(request, 'login.html', context)
-
-=======
             messages.error(request,'Invalid Login Parameters')
             return redirect('login')
         
     context={}
     return render(request,'login.html',context)
->>>>>>> db3adf6 (error in forms.py)
 
 def account_logout(request):
     if request.user.is_authenticated:
         logout(request)
-        messages.success(request, "Thanks for Checking on CMS Goodbye!!")
+        messages.success(request,"Thanks for Checking on CMS Goodbye!!")
         return redirect('login')
 
     else:
-        messages.error(
-            request, "You need to Login to be able to accsss this Software")
+        messages.error(request,"You need to Login to be able to accsss this Software")
         return redirect('login')
+    
+
 
 @unauthenticated
 def register(request):
-<<<<<<< HEAD
-    userForm = CustomUserForm(request.POST or None)
-    if request.method == 'POST':
-        if userForm.is_valid():
-            user = userForm.save(commit=False)
-            user.cms = user
-            user.save()
-            messages.success(
-                request, 'Account Created Successfully.You can now login')
-=======
     userForm=CustomUserForm(request.POST or None)
     if request.method=='POST':
     
@@ -244,19 +173,22 @@ def register(request):
             user.admin=admin
             user.cms=user
             messages.success(request,'Account Created for' + str(user))
->>>>>>> db3adf6 (error in forms.py)
             return redirect('login')
 
         else:
-<<<<<<< HEAD
-            messages.error(request, "Provided data Failed Validation")
-=======
             messages.error(request,'Invalid Parameters')
         
     
->>>>>>> db3adf6 (error in forms.py)
 
-    context = {
-        'form': userForm
+   
+    context={
+        'form':userForm
     }
-    return render(request, 'register.html', context)
+    return render(request,'register.html',context)
+
+
+
+    
+    
+        
+
